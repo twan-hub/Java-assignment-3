b6efc56ae3f8d02c235b17237d97230f6e01036b
twan-hub <adstith99@gmail.com>
2020-08-02 11:14:52 -0700

    finished

---
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -0,0 +1,42 @@
+package org.launchcode.javawebdevtechjobsmvc.controllers;
+
+import org.launchcode.javawebdevtechjobsmvc.models.Job;
+import org.launchcode.javawebdevtechjobsmvc.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    @PostMapping("results")
+
+    public String displaySearchResults(Model model, @RequestParam String searchTerm,@RequestParam String searchType) {
+        ArrayList<Job> jobs = new ArrayList<>();
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
+}
